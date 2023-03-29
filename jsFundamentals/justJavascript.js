1136872d546278a0bb4934d2c64df0dc68c56a8d
unknown <Liya Mabona>
2023-03-29 16:09:08 +0200

    jsFundamentals

---
+++ jsFundamentals/justJavascript.js
@@ -0,0 +1,80 @@
+// var a = 1;
+// var a = 10;
+// let a = 27;
+// let a = 10;
+
+// let a = 1
+// let b = 2
+// let c = a+b
+
+// console.log c (a+b)
+
+
+// let a = "10"
+// let b = 10;
+// if (a==10){
+// console.log ("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+/*let a = "10"
+let b = 10;
+if (a===10){
+console.log ("true");
+}
+else{
+    console.log("false");
+}
+*/
+
+/*let a = "10"
+let b = 10;
+if (a!=b){
+console.log ("true");
+}
+else{
+    console.log("false");
+}*/ 
+
+/*let d = 10
+let c = 10
+let a = 10;
+let b = 20;
+
+if(a==b&& c== d)
+{console.log("true")}else{
+    console.log("false");
+}*/
+
+/*let a = 1;
+let b = 2;
+let c 
+  console.log(a);
+  console.log(b);
+*/
+
+/*let a = - 5;
+
+if (a>10){
+    console.log('Greater than a dacade');
+}else if(a==10){
+console.log("it is a decade")
+}else if(a<10 && a >=0){
+    console.log("it is lesser than a decade")
+}else{
+    console.log("We cannot have negative years")
+}*/
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 2;
+    if(c>b)
+    {console.log("the maximum value is c")
+}
+    if(b===d){console.log("the values are strictly equal")}
+    else{
+    console.log("wrong")
+}
