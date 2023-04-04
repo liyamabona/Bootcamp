f5013189ec212cee9b046de40ed485e720ca8f6c
unknown <Liya Mabona>
2023-04-04 15:58:45 +0200

    tempCon

---
+++ AgeCheck/age.js
@@ -0,0 +1,16 @@
+// function ageChecker(birthyear,currentyear){
+
+//     let Age = currentyear - birthyear
+    
+    
+//     if (Age < 0){
+    
+//         console.log("age cannot be a nagetive");
+    
+    
+//     }else
+    
+//     console.log(Age)
+//     }
+    
+//     ageChecker(2019,2023)
---
+++ AgeCheck/empty.js
@@ -0,0 +1,22 @@
+// function add10(a){
+
+//  let i = a + 10
+//  console.log(i)
+// }
+//  add10(10)
+
+// function multiplyby5(b){
+
+// let j = b * 5
+
+// console.log(j)
+// }
+// multiplyby5(5)
+
+
+// function sumofTwo(e,f){
+
+// let k = e + f
+// return k 
+// }
+// console.log(sumofTwo(15,20))
