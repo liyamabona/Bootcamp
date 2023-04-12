d4308edfa4f8f16c975db1304605bd25ae45d4bc
unknown <Liya Mabona>
2023-04-12 16:06:27 +0200

    temp.js

--- tempCon/temp.js
+++ tempCon/temp.js
@@ -16,17 +16,28 @@
 
 function cTf(){
 
-    let fah =(document.getElementById("cTf").value)
+    let fah =document.getElementById("cTf").value
+    let feedback =document.getElementById("feedback")
     let celsius = (fah -32) * (5/9);
+    feedback.innerHTML = celsius
 
-    if (celsius < 16 ){
-        alert(celsius + "" +  ",it is cold")
+
+    if(!fah){
+    feedback.innerHTML =("please enter value")
+    }
+    // if(celsius >0 ){
+    //     // alert("please enter value")
+    //     feedback.innerHTML =("please enter value")
+    // }
+    else if (celsius < 16 ){
+        // alert(celsius + "" +  ",it is cold")
+        feedback.innerHTML =(celsius + "" +  ",it is cold")
     } else if(celsius >= 16 && celsius <21){
-        alert(celsius + " "  +  ",wear Tshirt and jeans")
+        // alert(celsius + " "  +  ",wear Tshirt and jeans")
+        feedback.innerHTML =(celsius + " "  +  ",wear Tshirt and jeans")
     } else {
-
-        alert
-        (celsius + " "  +  "it is hot,wear shorts")
+        // alert(celsius + " "  +  "it is hot,wear shorts")
+        feedback.innerHTML =(celsius + " "  +  "it is hot,wear shorts")
     }
     }
     // cTf()
