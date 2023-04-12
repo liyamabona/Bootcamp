4b4bb64c3b97e2ba757f505c8f05231e178234cd
unknown <Liya Mabona>
2023-04-12 16:04:41 +0200

    age.js

--- AgeCheck/age.js
+++ AgeCheck/age.js
@@ -7,9 +7,10 @@ if(!birthyear){
     alert("please enter your birthyear !");
 }else if(Age < 0){
     alert("age cannot be a nagetive");
-
-}else{
+}else if(birthyear < 0)
+    alert("birthyear cannot be a nagetive")
+else
     alert(Age);
 }
-}
+
 ageChecker()
